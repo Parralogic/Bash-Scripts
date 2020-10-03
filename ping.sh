59f5bf9d51f312b8dadb2b600b69ba6246d54256
Parralogic <Parralogic@gmail.com>
2020-10-03 10:39:18 -0700

    10/03/2020

--- ping.sh
+++ ping.sh
@@ -4,15 +4,16 @@ while true; do
 clear
 echo -e "\e[31mNow pinging ROUTER, MODEM and WWW\e[00m"
 echo
-ping -c 1 192.168.0.1 2> /dev/null  | echo "Router: $(grep ttl)"
+ping -c 1 192.168.0.1 2> /dev/null  | echo -e "\e[32mRouter\e[00m: $(grep ttl)"
 if [[ $? = 0 ]]; then
+sleep 2
 echo
-ping -c 1 192.168.100.1 2> /dev/null | echo -e "Modem: $(grep ttl)"
+ping -c 1 192.168.100.1 2> /dev/null | echo -e "\e[32mModem\e[00m: $(grep ttl)"
 fi
 echo
 ping -c 3 www.google.com 1> /dev/null
 if [[ $? = 0 ]]; then
-echo -e "WWW:\e[92mSuccessful\e[00m"
+echo -e "\e[32mWWW\e[00m:\e[92mSuccessful\e[00m"
 curl ifconfig.me
 sleep 6
 clear
