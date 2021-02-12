42d60a294cdcb378ec367263d92f4a5b51f06d71
Parralogic <Parralogic@gmail.com>
2021-02-12 11:01:31 -0800

    02/12/2021

---
+++ permissions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Creator: David Parra-Sandoval
+#Date: 02/12/2021
+#Last Modified: 02/12/2021
+clear
+
+if [[ -z $@ ]]; then
+echo "Usage $0 FILE1 FILE2 FILE3...etc"
+
+else
+
+for FILE in $@; do
+if [[ ! -e $FILE ]];
+then
+echo -e "[\e[91m$FILE\e[00m] does not Exist!, check spelling."
+exit 1
+fi
+chmod 000 $FILE
+echo -e -n "USER|OWNER of the file [\e[91m$FILE\e[00m] will be able to [r]ead [w]rite e[x]ecute:? "; read OWNER
+read -p "GROUP [r]ead [w]rite e[x]ecute:? " GROUP
+read -p "OTHERS [r]ead [w]rite e[x]ecute:? " OTHERS
+chmod -v u+$OWNER $FILE && chmod -v g+$GROUP $FILE && chmod -v o+$OTHERS $FILE
+read -p "Press Enter" ; clear
+done
+fi
+
+
